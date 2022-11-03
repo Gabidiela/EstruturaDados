344c9c64aa6480ab7c6c84fbfed8b4e8526139ee
Gabriela Gomes <gamacarrera@gmail.com>
2022-11-03 00:46:26 -0300

    Iniciando implementação do exemplo de conjuntos

--- Conjuntos/src/Main.java
+++ Conjuntos/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Set;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Para o exemplo vamos criar um conjunto que representa uma sacola de frutas");
+        System.out.println("-------------------------------------------------");
+        //Criando o conjunto sacolaFrutas
+        Set<String> sacolaFrutas = new HashSet<>();
+
+        System.out.println("Inserindo Nomes para Frutas que entrarão na sacola");
+        sacolaFrutas.add("Maçã");
+        sacolaFrutas.add("Goiaba");
+        sacolaFrutas.add("Laranja");
+        sacolaFrutas.add("Uva");
+        sacolaFrutas.add("Abacaxi");
+        sacolaFrutas.add("Pêra");
+        sacolaFrutas.add("Tomate");
+
+        System.out.println(sacolaFrutas);
+        System.out.println("-------------------------------------------------");
+
     }
+
 }
