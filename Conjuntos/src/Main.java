ca65768d653bb7318ffd5cf0dd25466fcd9fea20
Gabriela Gomes <gabriela.gomes@dtidigital.com.br>
2022-11-03 22:58:27 -0300

    Exemplo completo de uso de Conjunto

--- Conjuntos/src/Main.java
+++ Conjuntos/src/Main.java
@@ -1,12 +1,14 @@
-import java.util.Set;
+
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Para o exemplo vamos criar um conjunto que representa uma sacola de frutas");
         System.out.println("-------------------------------------------------");
-        //Criando o conjunto sacolaFrutas
-        Set<String> sacolaFrutas = new HashSet<>();
+        //Criando o conjunto sacolaFrutas usando HashSet
+        HashSet<String> sacolaFrutas = new HashSet<>();
 
         System.out.println("Inserindo Nomes para Frutas que entrarão na sacola");
         sacolaFrutas.add("Maçã");
@@ -16,8 +18,40 @@ public class Main {
         sacolaFrutas.add("Abacaxi");
         sacolaFrutas.add("Pêra");
         sacolaFrutas.add("Tomate");
+        sacolaFrutas.add("Maçã");
+
 
         System.out.println(sacolaFrutas);
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("-------------------------------------------------");
+        TreeSet<Integer> OrdenaNumero = new TreeSet<>();
+
+        System.out.println("Inserindo Números para o Conjunto ordenado!");
+        OrdenaNumero.add(13);
+        OrdenaNumero.add(18);
+        OrdenaNumero.add(47);
+        OrdenaNumero.add(3);
+        OrdenaNumero.add(89);
+        OrdenaNumero.add(30);
+        OrdenaNumero.add(19);
+
+        System.out.println(OrdenaNumero);
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("-------------------------------------------------");
+        LinkedHashSet<Double> OrdenaInsercao = new LinkedHashSet<>();
+
+        System.out.println("Inserindo Números para o Conjunto ordenado conforme inserido");
+        OrdenaInsercao.add(1.3);
+        OrdenaInsercao.add(18.2);
+        OrdenaInsercao.add(4.7);
+        OrdenaInsercao.add(0.3);
+        OrdenaInsercao.add(89.7);
+        OrdenaInsercao.add(30.4);
+        OrdenaInsercao.add(19.6);
+
+        System.out.println(OrdenaInsercao);
         System.out.println("-------------------------------------------------");
 
     }
